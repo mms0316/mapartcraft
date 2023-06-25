4abad93503b6a19b2644542ff88a93cff88dabe5
mms <lurkingdeeply@gmail.com>
2023-06-25 19:04:58 -0300

    Changed/customized page header

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,7 @@ import { Link } from "react-router-dom";
 import "./header.css";
 
 class Header extends Component {
-  state = { contactInfoClassname: "contactInfoBlurred" };
-
-  // Reveal contact info
-  onContactInfoClick = (event) => {
-    const { getLocaleString } = this.props;
-    event.preventDefault();
-    if (window.confirm(getLocaleString("FAQ/HAVE-YOU-READ"))) {
-      alert(getLocaleString("FAQ/HELP-IN-ENGLISH"));
-      this.setState({ contactInfoClassname: "contactInfo" });
-    }
-  };
-
   render() {
-    const { contactInfoClassname } = this.state;
     const { getLocaleString } = this.props;
     return (
       <div className="header">
@@ -30,7 +17,7 @@ class Header extends Component {
             {getLocaleString("FAQ/VIDEO-TUTORIAL")}
           </a>
           <span> | </span>
-          <a href="https://github.com/rebane2001/mapartcraft" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/mms0316/mapartcraft" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
           <span> | </span>
@@ -55,29 +42,10 @@ class Header extends Component {
           </a>
           {getLocaleString("DESCRIPTION/5")}
           <br></br>
-          {getLocaleString("DESCRIPTION/6")}
-          <span className={contactInfoClassname} onClick={this.onContactInfoClick}>
-            (rebane2001#3716)
-          </span>
-          {getLocaleString("DESCRIPTION/7")}
-          <span className={contactInfoClassname} onClick={this.onContactInfoClick}>
-            (
-            <a href="https://www.reddit.com/message/compose/?to=rebane2001" target="_blank" rel="noopener noreferrer">
-              /u/rebane2001
-            </a>
-            )
-          </span>
-          {getLocaleString("DESCRIPTION/8")}
-          <a href="https://github.com/rebane2001/mapartcraft/issues" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          {"."}
-          <br></br>
           <em>
-            {getLocaleString("DESCRIPTION/9")}
-            <a href="/mapartcraft/classic" target="_blank" rel="noopener noreferrer">
-              {getLocaleString("DESCRIPTION/10")}
-            </a>
+            {getLocaleString("DESCRIPTION/6")}
+            <a href="https://rebane2001.com/mapartcraft/" target="_blank" rel="noopener noreferrer">MapartCraft</a>
+            {getLocaleString("DESCRIPTION/7")}
             {"."}
           </em>
           <br></br>
